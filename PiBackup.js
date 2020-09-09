05093c2f5b181245e294c8932d632bb80773ec17
hygy <hygyke@gmail.com>
2020-09-09 22:47:29 +0200

    code cleanup

--- PiBackup.js
+++ PiBackup.js
@@ -27,13 +27,13 @@ class PiBackup {
 
 		// Detect add/insert
 		usbDetect.on('add', this.addUSBhandler );
-		usbDetect.on('add:vid', function(device) { console.log('add', device); });
-		usbDetect.on('add:vid:pid', function(device) { console.log('add', device); });
+//		usbDetect.on('add:vid', function(device) { console.log('add', device); });
+//		usbDetect.on('add:vid:pid', function(device) { console.log('add', device); });
 
 		// Detect remove
 		usbDetect.on('remove', function(device) { console.log('remove', device); });
-		usbDetect.on('remove:vid', function(device) { console.log('remove', device); });
-		usbDetect.on('remove:vid:pid', function(device) { console.log('remove', device); });
+//		usbDetect.on('remove:vid', function(device) { console.log('remove', device); });
+//		usbDetect.on('remove:vid:pid', function(device) { console.log('remove', device); });
 
 		// Detect add or remove (change)
 		//usbDetect.on('change', function(device) { console.log('change', device); });
@@ -55,7 +55,7 @@ class PiBackup {
 		// Start interval to check if the USB is mounted
 		this.checkUSBintervalID = setInterval(function() {
 			this.writeDebug(me);
-			me.drivelist.list(function(error, drives) {
+			this.drivelist.list(function(error, drives) {
 				console.log('listening for USB mount...');
 				if (error) { throw error; }
 
@@ -76,7 +76,7 @@ class PiBackup {
 					}
 				}
 			});
-		}, 1000);
+		}, 1000, this);
 	}
 }
 
